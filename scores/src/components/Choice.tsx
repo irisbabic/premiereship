730449e7e034deb58c294667dab0846ab56220ab
irisbabic <iris.babic@gmail.com>
2018-12-26 12:49:46 +0100

    Added comments and trend sort

--- scores/src/components/Choice.tsx
+++ scores/src/components/Choice.tsx
@@ -2,28 +2,39 @@ import * as React from 'react';
 import RankingTable from "./RankingTable";
 import * as data from "../data";
 import {StoreState} from "../types";
-//import './Choice.css'
 
 let clubs: Array<any> = [];
+let trendClubs: Array<any>;
 
 export interface Props {
     roundChoice?: (round: number) => number;
     state: StoreState;
 }
 
+
+/**
+ * @class Choice
+ * @classdesc Component for renderinf choice button and table components
+ */
 class Choice extends React.Component <Props> {
 
-    gamesWon = (klub: string, roundMatches: Object): number => {
+    /**
+     * @description Counts the number of games a team has won and returns that number
+     * @param club - club name
+     * @param roundMatches - all matches in the chosen round
+     * @returns {number} wins
+     */
+    gamesWon = (club: string, roundMatches: Object): number => {
         let wins = 0;
 
         (roundMatches as (Object)[]).map((match: Object, i: number) => {
-            if (klub === Object.keys(match)[0]) {
+            if (club === Object.keys(match)[0]) {
 
-                if (match[klub] > match[Object.keys(match)[1]]) {
+                if (match[club] > match[Object.keys(match)[1]]) {
                     wins++;
                 }
-            } else if ((klub === Object.keys(match)[1])) {
-                if (match[klub] > match[Object.keys(match)[0]]) {
+            } else if ((club === Object.keys(match)[1])) {
+                if (match[club] > match[Object.keys(match)[0]]) {
                     wins++;
                 }
             }
@@ -34,16 +45,22 @@ class Choice extends React.Component <Props> {
         return wins;
     };
 
-    gamesLost = (klub: string, roundMatches: Object): number => {
+    /**
+     * @description Counts the number of games a team has lost and returns that number
+     * @param club - club name
+     * @param roundMatches - all matches in the chosen round
+     * @returns {number} lost
+     */
+    gamesLost = (club: string, roundMatches: Object): number => {
         let lost = 0;
 
         (roundMatches as (Object)[]).map((match: Object, i: number) => {
-            if (klub === Object.keys(match)[0]) {
-                if (match[klub] < match[Object.keys(match)[1]]) {
+            if (club === Object.keys(match)[0]) {
+                if (match[club] < match[Object.keys(match)[1]]) {
                     lost++;
                 }
-            } else if ((klub === Object.keys(match)[1])) {
-                if (match[klub] < match[Object.keys(match)[0]]) {
+            } else if ((club === Object.keys(match)[1])) {
+                if (match[club] < match[Object.keys(match)[0]]) {
                     lost++;
                 }
             }
@@ -54,17 +71,23 @@ class Choice extends React.Component <Props> {
         return lost;
     };
 
-    gamesDrawn = (klub: string, roundMatches: Object): number => {
+    /**
+     * @description Counts the number of draws for a team and returns that number
+     * @param club - club name
+     * @param roundMatches - all matches in the chosen round
+     * @returns {number} drawn
+     */
+    gamesDrawn = (club: string, roundMatches: Object): number => {
         let drawn = 0;
 
         (roundMatches as (Object)[]).map((match: Object, i: number) => {
 
-            if (klub === Object.keys(match)[0]) {
-                if (match[klub] === match[Object.keys(match)[1]]) {
+            if (club === Object.keys(match)[0]) {
+                if (match[club] === match[Object.keys(match)[1]]) {
                     drawn++;
                 }
-            } else if ((klub === Object.keys(match)[1])) {
-                if (match[klub] === match[Object.keys(match)[0]]) {
+            } else if ((club === Object.keys(match)[1])) {
+                if (match[club] === match[Object.keys(match)[0]]) {
                     drawn++;
                 }
             }
@@ -75,16 +98,22 @@ class Choice extends React.Component <Props> {
         return drawn;
     };
 
-    ga = (klub: string, roundMatches: Object): number => {
+    /**
+     * @description Counts the number of goals scored against the team and returns that number
+     * @param club - club name
+     * @param roundMatches - all matches in the chosen round
+     * @returns {number} ga
+     */
+    ga = (club: string, roundMatches: Object): number => {
         let ga = 0;
 
         (roundMatches as (Object)[]).map((match: Object, i: number) => {
-            if (klub === Object.keys(match)[0]) {
-                if (match[klub] < match[Object.keys(match)[1]]) {
+            if (club === Object.keys(match)[0]) {
+                if (match[club] < match[Object.keys(match)[1]]) {
                     ga += match[Object.keys(match)[1]];
                 }
-            } else if ((klub === Object.keys(match)[1])) {
-                if (match[klub] < match[Object.keys(match)[0]]) {
+            } else if ((club === Object.keys(match)[1])) {
+                if (match[club] < match[Object.keys(match)[0]]) {
                     ga += match[Object.keys(match)[0]];
                 }
             }
@@ -95,13 +124,19 @@ class Choice extends React.Component <Props> {
         return ga;
     };
 
-    gf = (klub: string, roundMatches: Object): number => {
+    /**
+     * @description Counts the number of goals scored by the team and returns that number
+     * @param club - club name
+     * @param roundMatches - all matches in the chosen round
+     * @returns {number} gf
+     */
+    gf = (club: string, roundMatches: Object): number => {
         let gf = 0;
 
         (roundMatches as (Object)[]).map((match: Object, i: number) => {
             Object.keys(match).map((x, i) => {
-                if (klub === x) {
-                    gf = match[klub];
+                if (club === x) {
+                    gf = match[club];
                 }
                 return 1;
             });
@@ -110,23 +145,51 @@ class Choice extends React.Component <Props> {
         return gf;
     };
 
+    /**
+     * @description Initializes the club array for each chosen round and makes a new one
+     * @param roundMatches - all matches in the chosen round
+     */
     handleClubs = (roundMatches: Object): void => {
         clubs = [];
 
         (roundMatches as (Object)[]).forEach((match: Object) => clubs.push(Object.keys(match)));
     };
-    calculateTrend = (klub: string, roundMatches: Object) => {
-        if (this.gamesWon(klub, roundMatches) === 1) {
+
+    /**
+     * @description Checks the outcome for a given game
+     * @param club - club name
+     * @param roundMatches - all matches in the chosen round
+     * @returns {string} W || D || L
+     */
+    calculateTrend = (club: string, roundMatches: Object) => {
+        if (this.gamesWon(club, roundMatches) === 1) {
             return "W"
-        } else if (this.gamesDrawn(klub, roundMatches) === 1) {
+        } else if (this.gamesDrawn(club, roundMatches) === 1) {
             return "D"
         } else {
             return "L"
         }
     };
 
+    /**
+     * @description Sorts an array of team names
+     * @param roundMatches - all matches in the chosen round
+     */
+    sortTrend(roundMatches: Object){
+        trendClubs = [];
+
+        (roundMatches as (Object)[]).forEach((match?: any) => {
+            (Object.keys(match)).map( (club: string) => trendClubs.push(club))
+                });
+        trendClubs = trendClubs.sort();
+    }
+
+    /**
+     * @description Renders choice button and other components
+     */
     render() {
         this.handleClubs(data[this.props.state.round - 1].matches);
+        this.sortTrend(data[0].matches);
         return (
             <div className="container">
                 <div className="row">
@@ -150,11 +213,9 @@ class Choice extends React.Component <Props> {
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-md center-block">
+                    <div className="col-lg">
                     <h3 className="text-center">Round: {this.props.state.round}</h3>
                 </div>
-                    <div className="col-md center-block">
-                    </div>
                 </div>
                 <div className="row">
                     <div className="col-md">
@@ -199,8 +260,8 @@ class Choice extends React.Component <Props> {
                             </tr>
                             </thead>
                             <tbody>
-                            {clubs.sort().map((clubs?: any, index?: number) =>
-                                clubs.map((club: string, i: number) =>
+                            {trendClubs.map((club?: any) =>
+                               // clubs.sort().map((club: string) =>
                                     <tr key={club}>
                                         <td>{club}</td>
                                         <td>{this.calculateTrend(club, data[data.length - 1].matches)}</td>
@@ -209,7 +270,7 @@ class Choice extends React.Component <Props> {
                                         <td>{this.calculateTrend(club, data[data.length - 4].matches)}</td>
                                         <td>{this.calculateTrend(club, data[data.length - 5].matches)}</td>
                                     </tr>
-                                )
+                                //)
                             )
                             }
                             </tbody>
