934dcbcd8d7dba507ad0578831b4bef3796c3eca
irisbabic <iris.babic@gmail.com>
2018-12-26 14:34:51 +0100

    Alphabetized imports

--- scores/src/index.tsx
+++ scores/src/index.tsx
@@ -1,24 +1,26 @@
+import * as data from "./data";
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
 import App from './components/App';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
 import round from './reducers/index';
-import { Provider } from "react-redux";
-import * as data from "./data";
+
+import './index.css';
+
+import {createStore} from 'redux';
+import {Provider} from "react-redux";
 
 
-
-const store = createStore(round,{
+const store = createStore(round, {
     round: data.length,
 });
 
-//store.dispatch(changeRound(5));
 ReactDOM.render(
     <Provider store={store}>
-  <App />,
+        <App/>,
     </Provider>,
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
 registerServiceWorker();
