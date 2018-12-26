ad5f76bde12b4c69a58f466672acec57a779096d
irisbabic <iris.babic@gmail.com>
2018-12-26 13:23:03 +0100

    Added comments

--- scores/src/components/RankingTable.tsx
+++ scores/src/components/RankingTable.tsx
@@ -11,10 +11,16 @@ export interface Props {
 
 class RankingTable extends React.Component <Props>{
 
+    /**
+     * @description When component mounts, helper function sortData is called
+     */
 componentDidMount(): void {
     sortData();
     }
 
+    /**
+     * @description Renders rows in a table depending on current round
+     */
     render(): React.ReactNode {
 
         return (<tr key={this.props.club}>
@@ -54,26 +60,33 @@ componentDidMount(): void {
 
 export default RankingTable;
 
+/**
+ * @description Sorts given data in table
+ */
 function sortData(): void {
-    // Read table body node.
+
     let table = document.getElementById('myTable')!.getElementsByTagName('tbody').item(0);
-    // Read table row nodes.
     let rowData = table!.rows;
 
+    //Sorting by total point count
+    //Looping through row nodes to access table data
     for (let i = 0; i < rowData.length - 1; i++) {
         for (let j = 0; j < rowData.length - (i + 1); j++) {
-            //Swap row nodes if short condition matches
+
+            //Swapping row nodes if condition is satisfied
             if (parseInt(rowData.item(j)!.getElementsByTagName('td').item(8)!.innerHTML) < parseInt(rowData.item(j + 1)!.getElementsByTagName('td').item(8)!.innerHTML)) {
                 table!.insertBefore(rowData.item(j + 1)!, rowData.item(j));
+
+                //Return ranking number to to previous state
                 rowData.item(j)!.getElementsByTagName('th').item(0)!.innerHTML = (rowData.item(j)!.rowIndex).toString();
                 rowData.item(j + 1)!.getElementsByTagName('th').item(0)!.innerHTML = (rowData.item(j + 1)!.rowIndex).toString();
             }
         }
     }
 
+    //If there are teams with same total point count, sort by total point count and goal difference
     for(let i = 0; i < rowData.length - 1; i++) {
         for(let j = 0; j < rowData.length - (i + 1); j++) {
-            //Swap row nodes if short condition matches
             if(parseInt(rowData.item(j)!.getElementsByTagName('td').item(8)!.innerHTML) === parseInt(rowData.item(j+1)!.getElementsByTagName('td').item(8)!.innerHTML)) {
                 if(parseInt(rowData.item(j)!.getElementsByTagName('td').item(7)!.innerHTML) < parseInt(rowData.item(j+1)!.getElementsByTagName('td').item(7)!.innerHTML)) {
                     table!.insertBefore(rowData.item(j+1)!,rowData.item(j));
@@ -84,9 +97,9 @@ function sortData(): void {
         }
     }
 
+    //If there are teams with same total point count and goal difference, sort by total point count, goal difference and scored goals
     for(let i = 0; i < rowData.length - 1; i++) {
         for(let j = 0; j < rowData.length - (i + 1); j++) {
-            //Swap row nodes if short condition matches
             if(parseInt(rowData.item(j)!.getElementsByTagName('td').item(8)!.innerHTML) === parseInt(rowData.item(j+1)!.getElementsByTagName('td').item(8)!.innerHTML)) {
                 if(parseInt(rowData.item(j)!.getElementsByTagName('td').item(7)!.innerHTML) === parseInt(rowData.item(j+1)!.getElementsByTagName('td').item(7)!.innerHTML)) {
                     if(parseInt(rowData.item(j)!.getElementsByTagName('td').item(5)!.innerHTML) < parseInt(rowData.item(j+1)!.getElementsByTagName('td').item(5)!.innerHTML)) {
