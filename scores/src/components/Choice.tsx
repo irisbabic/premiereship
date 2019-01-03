bf733dae582be5662f371315445ed07ddc12625b
IrisBabic <iris.babic@gmail.com>
2019-01-03 23:41:33 +0100

    Update Choice.tsx

--- scores/src/components/Choice.tsx
+++ scores/src/components/Choice.tsx
@@ -86,10 +86,7 @@ class Choice extends React.Component <Props> {
                 if (match[club] === match[Object.keys(match)[1]]) {
                     drawn++;
                 }
-            } else if (club === Object.keys(match)[1]) {
-                if (match[club] === match[Object.keys(match)[0]]) {
-                    drawn++;
-                }
+              } 
             }
             return 1;
         });
